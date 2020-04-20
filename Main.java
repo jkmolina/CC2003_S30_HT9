8c0ca98686c7269b97340f55aed99241c522db3f
jkmolina <joonhodavid@gmail.com>
2020-04-20 16:39:50 -0600

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        String one="danielb";
+        String two="daniela";
+        System.out.println(one.compareTo(two));
+
+    }
+}
---
+++ SplayNode.java
@@ -0,0 +1,24 @@
+
+public class SplayNode {
+    SplayNode left, right, parent;
+
+    String english;
+    String spanish;
+
+
+
+
+    /** Constructor **/
+
+    public SplayNode(String english,String spanish)
+
+    {
+        this.left=null;
+        this.right=null;
+        this.parent=null;
+        this.english=english;
+        this.spanish=spanish;
+        
+    }          
+
+}
