0bb4a25673478e7d3dbda690fdd53e731179213c
pablofermarroquin <pablofermarroquin@gmail.com>
2020-04-24 15:58:36 -0600

    Profiler agregado

--- Main.java
+++ Main.java
@@ -1,19 +1,22 @@
+
 public class Main{
     public static void main(String[] args) {
+        while(true)
+        {
+            try
+            {
+                System.out.println("Se traducira el archivo texto.txt\n");
+                Translator translator= new Translator();
+                translator.PrepararDiccionario();
+                String traduccionFinal= translator.Traducir();
+                System.out.println(traduccionFinal);
+                
+            }
+            catch(Exception e)
+            {
 
-         try
-         {
-             System.out.println("Se traducira el archivo texto.txt\n");
-             Translator translator= new Translator();
-             translator.PrepararDiccionario();
-             String traduccionFinal= translator.Traducir();
-             System.out.println(traduccionFinal);
-         }
-         catch(Exception e)
-         {
-
-         }
-        
-
+            }
+            
+        }
     }
 }
